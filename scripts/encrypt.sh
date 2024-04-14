5585e35805dfe7dbe0237276b8fa973576a835ca
maany <imptodefeat@gmail.com>
2024-04-14 09:00:45 +0200

    add cloudflare tunnel for kernel planckster

--- scripts/encrypt.sh
+++ scripts/encrypt.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Directory where the files are located
-directory="releases/base/secrets"
+directory="releases/production/secrets"
 
 # Pattern to match
 pattern="*.yaml"
@@ -10,6 +10,11 @@ pattern="*.yaml"
 for file in "$directory"/$pattern
 do
   echo $file
+  # Check if the file name is "kustomization.yaml"
+  if [ "$(basename "$file")" = "kustomization.yaml" ]; then
+    echo "Skipping kustomization.yaml"
+    continue
+  fi
   # Encrypt the file and save the output to a new file
   sops --encrypt --in-place --age=age19xkz3rtp7pu0vzxmzqq2qd5reh0e4v328764re7vtj547yxls3zqgv6mp3 \
       --encrypted-regex '^(data|stringData)$'  "$file"
